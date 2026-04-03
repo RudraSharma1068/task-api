1e49d6fe005394e18b6cae68cb05282921151384
Rudra Sharma <rudrasharma56449@gmail.com>
2026-04-03 18:44:53 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ const taskRoutes = require('./routes/tasks');
 
 const app = express();
 app.use(express.json());
+
 app.use('/tasks', taskRoutes);
 
+// Root route for Render
+app.get('/', (req, res) => {
+  res.send('Task API is running');
+});
+
+// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal server error' });
